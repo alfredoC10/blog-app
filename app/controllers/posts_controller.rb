289cb3b6c368d94e5d2c545e1f819fef82ef4f93
Huemac Alfredo Córdova <hmca101292@outlook.com>
2021-01-27 19:26:00 -0600

    Create edit.html.erb

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -8,15 +8,22 @@ class PostsController < ApplicationController
   end
 
   def new
-
+    @post = Post.new
   end
 
   def create
     #render plain: params[:post].inspect
     @post = Post.new(post_params)
 
-    @post.save
-    redirect_to @post
+    if(@post.save)
+      redirect_to @post
+    else
+      render 'new'      
+    end
+  end
+
+  def edit
+    @post = Post.find(params[:id])
   end
 
   private def post_params
